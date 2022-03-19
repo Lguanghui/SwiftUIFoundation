ab65dd96ba6310332f1cffe41d6e34c2bb58a558
liangguanghui <liangguanghui@keep.com>
2022-03-19 23:56:22 +0800

    init

---
+++ Tests/SwiftUIKitTests/SwiftUIKitTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import SwiftUIKit
+
+final class SwiftUIKitTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(SwiftUIKit().text, "Hello, World!")
+    }
+}
